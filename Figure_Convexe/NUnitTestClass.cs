c85f33264b85c4ce13abb72c0728790ef5d5a00c
victorsmits <victor.smits@student.vinci.be>
2018-10-30 15:13:40 +0100

    update

---
+++ Figure_Convexe/NUnitTestClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+namespace Figure_Convexe
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void TestGetArea()
+        {
+            int[,] image = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+            };
+
+            int[,] Boundary = {
+                {-1, 2, 1, 1, 2, 2, 3, -1},
+                {-1, 4, 5, 5, 5, 4, 4, -1}
+            };
+
+            Assert.AreEqual(22, MainClass.GetArea(image));
+        }
+    }
+}
--- Figure_Convexe/Program.cs
+++ Figure_Convexe/Program.cs
@@ -6,7 +6,36 @@ namespace Figure_Convexe
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] image = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+            };
+        }
+
+        /*
+        * @pre image != null
+        * image only contains 0’s and 1’s and the set of 1’s is a convex image
+        * @post The returned value contains the area of the image as the number of pixels
+        */
+        public static int GetArea(int[,] image)
+        {
+            int count = 0;
+            foreach (int elem in image)
+            {
+                count += elem;
+            }
+            return count;
+        }
+
+        public static int[,] GetBoundary(int[,] image)
+        {
+            return null;
         }
     }
 }
